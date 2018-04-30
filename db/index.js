a8c6e9a52bce093f800e9ecaf2257f423227294b
Geena Gao <geena.gao@gmail.com>
2018-04-30 17:39:23 -0400

    Changed db url

--- db/index.js
+++ db/index.js
@@ -4,10 +4,13 @@ const Sequelize = require('sequelize');
 const pkg = require('../package.json');
 
 console.log(chalk.yellow("Opening database connection"));
+const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
 // create the database instance that can be used in other database files
-module.exports = new Sequelize(`postgres://localhost:5432/${pkg.name}`, {
-  logging: false, // so we don't see all the SQL query made
-});
-
+module.exports = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  {
+    logging: false
+  }
+)
 // don't forget to run our models files and make all associations for our Sequelize objects (if you do it here consider circular references)
